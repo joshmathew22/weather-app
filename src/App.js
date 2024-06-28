7db05b13936a8bd5dff8124cde8b809b376cf204
Josh Mathew <joshmathew2022@gmail.com>
2024-06-28 15:10:03 -0500

    connect api with search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React,{useState} from "react";
 import axios from "axios";
 function App() {
+  const[data,setData] = useState({})
+  const[location,setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=84d0410f4bf7a805035c226e4c45cd74`
 
 
-  //const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=84d0410f4bf7a805035c226e4c45cd74'
-
-
+  const searchLocation = (event) =>{
+    if(event.key==='Enter'){
+      axios.get(url).then((response)=>{
+        setData(response.data)
+        console.log(response.data)
+      })
+    }
+    
+  }
   return (
     <div className="app">
+      <div className="search">
+        <input
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type="text"
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
