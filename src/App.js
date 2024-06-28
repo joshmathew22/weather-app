8230e5712f546476802d1525709109135e308aa8
Josh Mathew <joshmathew2022@gmail.com>
2024-06-28 15:31:07 -0500

    get all data from api

--- src/App.js
+++ src/App.js
@@ -30,29 +30,33 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Dallas</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>80 F</h1>
+            {data.main ?<h1>{data.main.temp} F</h1>: null}
           </div>
           <div className="description">
-            <p>Clouds</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
+
+        {data.name != undefined &&
         <div className="bottom">
-          <div className="feels">
-            <p className="bold">85 F</p>
-            <p>Feels Like</p>
-          </div>
-          <div className="humidity">
-            <p className="bold"> 15%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <p className="bold"> 10 MPH</p>
-            <p>Wind Speed</p>
-          </div>
+        <div className="feels">
+          {data.main ? <p className="bold">{data.main.feels_like} F</p> : null}
+          <p>Feels Like</p>
         </div>
+        <div className="humidity">
+          {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+          <p>Humidity</p>
+        </div>
+        <div className="wind">
+          {data.wind ? <p className="bold">{data.wind.speed} MPH</p> : null}
+          <p>Wind Speed</p>
+        </div>
+      </div>
+        }
+        
       </div>
     </div>
   );
