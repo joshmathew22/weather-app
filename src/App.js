aecae91174158749b57668f306806938d3755f6b
Josh Mathew <joshmathew2022@gmail.com>
2024-06-28 14:48:30 -0500

    modify css

--- src/App.js
+++ src/App.js
@@ -7,8 +7,34 @@ function App() {
 
 
   return (
-    <div className="App">
-     bye
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>80 F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">85 F</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold"> 15%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold"> 10 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
